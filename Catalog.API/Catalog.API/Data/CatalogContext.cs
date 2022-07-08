544f47a63a9dbf50161d4dc6489b73d2ccd69f08
Eduardo Barros <eduardo.santos@acbprocessamento.com.br>
2022-07-08 17:26:52 -0300

    criando projeto curso microservice macoratti

---
+++ Catalog.API/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,22 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration config)
+        {
+            var client = new MongoClient(config.GetValue<string>(
+                "databaseSettings: ConnectionString"));
+
+            var database = client.GetDatabase(config.GetValue<string>(
+            "databaseSettings: DatabaseName"));
+
+            Products = database.GetCollection<Product>(config.GetValue<string>(
+            "databaseSettings: CollectionName"));
+            CatalogContextSeed.SeedData(Products);
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
---
+++ Catalog.API/Catalog.API/Repository/ProductRepository.cs
@@ -0,0 +1,48 @@
+﻿using Catalog.API.Data;
+using Catalog.API.Entities;
+
+namespace Catalog.API.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        readonly ICatalogContext _context;
+        public ProductRepository(ICatalogContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Product> GetProduct(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductByCategory(string categoryName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            throw new NotImplementedException();
+        }
+        public async Task CreateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> DeleteProduct(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> UpdateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
